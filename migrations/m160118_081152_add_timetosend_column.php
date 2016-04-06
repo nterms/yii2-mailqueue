7b39394485b936c3a568ec842811f8f38191e368
PeterKhoa <peterkhoa@gmail.com>
2016-04-06 17:24:25 +0700

    Fix bug in migrations add_timetosend_column

--- migrations/m160118_081152_add_timetosend_column.php
+++ migrations/m160118_081152_add_timetosend_column.php
@@ -1,18 +1,19 @@
 <?php
 
 use yii\db\Migration;
+use nterms\mailqueue\MailQueue;
 
 class m160118_081152_add_timetosend_column extends Migration
 {
     public function up()
     {
-        $this->addColumn('mail_queue', 'time_to_send', $this->dateTime()->notNull());
-        $this->createIndex('IX_time_to_send', 'mail_queue', 'time_to_send');
+        $this->addColumn(Yii::$app->get(MailQueue::NAME)->table, 'time_to_send', $this->dateTime()->notNull());
+        $this->createIndex('IX_time_to_send', Yii::$app->get(MailQueue::NAME)->table, 'time_to_send');
     }
 
     public function down()
     {
-        $this->dropIndex('IX_time_to_send', 'mail_queue');
-        $this->dropColumn('mail_queue', 'time_to_send');
+        $this->dropIndex('IX_time_to_send', Yii::$app->get(MailQueue::NAME)->table);
+        $this->dropColumn(Yii::$app->get(MailQueue::NAME)->table, 'time_to_send');
     }
 }
