721ea9e947c79cc288edac299a3dd327bf23c7c8
Bert Brunekreeft <BBrunekreeft@gmail.com>
2016-11-11 09:20:39 +0100

    Create column `swift_message`
    Column is used for storing serialized Swift_Message object

---
+++ migrations/m161111_080914_add_swift_message_column_to_mail_queue_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding swift_message to table `mail_queue`.
+ */
+class m161111_080914_add_swift_message_column_to_mail_queue_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->addColumn('mail_queue', 'swift_message', $this->longtext());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropColumn('mail_queue', 'swift_message');
+    }
+}
