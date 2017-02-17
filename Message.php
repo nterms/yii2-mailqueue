3c756f6c5fd8d80735bc0cebbbd73e49f59b2119
Bert Brunekreeft <BBrunekreeft@gmail.com>
2017-02-17 12:40:09 +0100

    Removed obsolete fields
    Fields from, to, cc, bcc, html_body, text_body, reply_to and charset are not used anymore.

--- Message.php
+++ Message.php
@@ -31,40 +31,10 @@ class Message extends \yii\swiftmailer\Message
 
         $item = new Queue();
 
-        $item->from = serialize($this->from);
-        $item->to = serialize($this->getTo());
-        $item->cc = serialize($this->getCc());
-        $item->bcc = serialize($this->getBcc());
-        $item->reply_to = serialize($this->getReplyTo());
-        $item->charset = $this->getCharset();
         $item->subject = $this->getSubject();
         $item->attempts = 0;
         $item->swift_message = base64_encode(serialize($this));
         $item->time_to_send = date('Y-m-d H:i:s', $time_to_send);
-
-        $parts = $this->getSwiftMessage()->getChildren();
-        // if message has no parts, use message
-        if ( !is_array($parts) || !sizeof($parts) ) {
-            $parts = [ $this->getSwiftMessage() ];
-        }
-
-        foreach( $parts as $part ) {
-            if( !( $part instanceof \Swift_Mime_Attachment ) ) {
-                /* @var $part \Swift_Mime_MimeEntity */
-                switch( $part->getContentType() ) {
-                    case 'text/html':
-                        $item->html_body = $part->getBody();
-                    break;
-                    case 'text/plain':
-                        $item->text_body = $part->getBody();
-                    break;
-                }
-
-                if( !$item->charset ) {
-                    $item->charset = $part->getCharset();
-                }
-            }
-        }
 
         return $item->save();
     }
